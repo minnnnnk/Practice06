02e20b67d7ec05eef672850188a3b4416a37ec9d
mingyu <jubal9153@gmail.com>
2022-04-27 18:54:42 +0900

    2022-04-27

--- src/com/javaex/ex04/Employee.java
+++ src/com/javaex/ex04/Employee.java
@@ -1,9 +1,29 @@
 package com.javaex.ex04;
 
-public class Employee {
+public class Employee extends User{
+	//필드
+	private int salary;
+	//생성자
+	public Employee() {
+		
+	}
+	
+	public Employee(String id, String password, String name, int salary) {
+		super(id, password, name);
+		this.salary = salary;
+	}
 
+	//메소드 gs
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
 	
-	
-
-	
+	//메소드 일반
+	public void showInfo() {
+		System.out.println("#아이디:"+id+", #패스워드: "+password+", #이름: "+name+", #월급:"+salary  );
+		System.out.println("운영자의 월급은 "+salary+ "원 입니다.");
+	}
 }
--- src/com/javaex/ex04/UserApp.java
+++ src/com/javaex/ex04/UserApp.java
@@ -4,7 +4,19 @@ public class UserApp {
 
 	public static void main(String[] args) {
 		
-
+		User[] uArray = new User[3];
+		
+		Customer c1 = new Customer("jws","j1234","정우성",1000);
+		Customer c2 = new Customer("yjs","y2345","이효리",2000);
+		Employee e1 = new Employee("master","m7788","운영자",5000000);
+		
+		uArray[0] = c1;
+		uArray[1] = c2;
+		uArray[2] = e1;
+		
+		for(int i = 0; i<uArray.length; i++) {
+			uArray[i].showInfo();
+		}
 	}
 
 }
