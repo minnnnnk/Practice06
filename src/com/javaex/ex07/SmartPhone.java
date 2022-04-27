6b0e8a60588e2cb23fcd409ce591d5147f2e2139
mingyu <jubal9153@gmail.com>
2022-04-27 20:17:00 +0900

    2022-04-27

--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -3,31 +3,28 @@ package com.javaex.ex07;
 public class SmartPhone extends MusicPhone {
     
     public void execute(String str){
+    	 //코드작성
 		if(str.equals("앱")) {
 				app();
         } else if(str.equals("음악")) {
         		music();
         } else {
-        	call();
+        		super.execute(str);;
         }
-        //코드작성
      
     
     
     }
+
+    //메소드작성
  	public void app () {
  		System.out.println("앱실행");
  	}
- 	
+ 	//메소드작성
  	public void music () {
  		System.out.println("다운로드해서 음악재생");
  	}
- 	public void call () {
- 		System.out.println("통화기능시작");
- 	}
-    //메소드작성
-    
-    //메소드작성
+ 	
     
 }    
     
